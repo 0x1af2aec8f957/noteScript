1b711f159fc148f5e1ad732d16c94603b056b2b4
chuan.pro <603803799@qq.com>
2018-01-10 23:55:51 +0800

    修复default布局错误

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1' // width=device-width, initial-scale=1
       },
       {
         name: 'author',
